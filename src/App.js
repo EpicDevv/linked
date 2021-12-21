1c704ee33f2190c2b55604e8c8c994ef9d00dceb
Meow Security <meowsecurity@Meows-MacBook-Pro.local>
2021-12-20 22:12:23 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import github from "./assets/gh.png";
+import linkedin from "./assets/li.png";
+import instagram from "./assets/in.png";
+import twitter from "./assets/twitter.png";
+import youtube from "./assets/youtube.png";
+import discord from "./assets/discord.png";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div className="container">
+      <div className="heading">
+        <h1 className="title"> Epic Dev </h1>
+      </div>
+
+      <div className="social-icons-container">
+        <a href="https://github.com/EpicDevv" className="social-icon">
+          <img src={github} alt="social" />
         </a>
-      </header>
+        <a
+          href="https://www.linkedin.com/in/zayne-lovecraft-0ba3a621a/"
+          className="social-icon"
+        >
+          <img src={linkedin} alt="social" />
+        </a>
+        <a href="https://www.instagram.com/script_daddy/" className="social-icon">
+          <img src={instagram} alt="social" />
+        </a>
+        <a href="https://twitter.com/epic_devv" className="social-icon">
+          <img src={twitter} alt="social" />
+        </a>
+        <a
+          href="https://www.youtube.com/channel/UCSm3TslJkZBoYs-qFC9G_4Q"
+          className="social-icon"
+        >
+          <img src={youtube} alt="social" />
+        </a>
+
+        <a
+          href="https://www.facebook.com/profile.php?id=100071547166443"
+          className="social-icon"
+        >
+          <img src={discord} alt="social" />
+        </a>
+      </div>
+      <div className="bio">
+        <p className="bio-text">Follow to join the #1 coding community !</p>
+       
+      </div>
+
     </div>
   );
 }
